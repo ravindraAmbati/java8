0007b7a9dc1adbd1e6d5741ecf1acd7a9cdd33f4
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-02 13:00:07 +0530

    impl student biconsumer, closes #12

--- src/main/java/com/learn/java8/bootstrap/StudentBootstrap.java
+++ src/main/java/com/learn/java8/bootstrap/StudentBootstrap.java
@@ -12,13 +12,13 @@ public class StudentBootstrap {
         Student s1 = Student.builder().name("Leo").gender("Male").grade(1).gpa(8.0d).activities(Arrays.asList("singing", "aerobics", "cooking")).build();
         Student s2 = Student.builder().name("Neo").gender("Female").grade(1).gpa(7.3d).activities(Arrays.asList("cycling", "basketball", "soccer")).build();
         Student s3 = Student.builder().name("Intell").gender("Male").grade(2).gpa(8.1d).activities(Arrays.asList("soccer", "swimming", "singing")).build();
-        Student s4 = Student.builder().name("Jack").gender("Female").grade(2).gpa(9.0d).activities(Arrays.asList("running", "basketball")).build();
-        Student s5 = Student.builder().name("Tech").gender("Male").grade(3).gpa(6.1d).activities(Arrays.asList("soccer", "cycling")).build();
-        Student s6 = Student.builder().name("Log").gender("Female").grade(3).gpa(5.9d).activities(Arrays.asList("swimming", "dancing")).build();
-        Student s7 = Student.builder().name("Novo").gender("Male").grade(4).gpa(7.0d).activities(Arrays.asList("singing", "running")).build();
-        Student s8 = Student.builder().name("Ipho").gender("Female").grade(4).gpa(8.0d).activities(Arrays.asList("cycling", "basketball")).build();
-        Student s9 = Student.builder().name("Mac").gender("Male").grade(5).gpa(7.5d).activities(Arrays.asList("soccer", "crafting")).build();
-        Student s0 = Student.builder().name("Cisco").gender("Female").grade(5).gpa(8.0d).activities(Arrays.asList("crafting", "aerobics")).build();
+        Student s4 = Student.builder().name("Jack").gender("Female").grade(2).gpa(9.0d).activities(Arrays.asList("running", "basketball", "crafting")).build();
+        Student s5 = Student.builder().name("Tech").gender("Male").grade(3).gpa(6.1d).activities(Arrays.asList("soccer", "cycling", "swimming")).build();
+        Student s6 = Student.builder().name("Log").gender("Female").grade(3).gpa(5.9d).activities(Arrays.asList("swimming", "dancing", "crafting")).build();
+        Student s7 = Student.builder().name("Novo").gender("Male").grade(4).gpa(7.0d).activities(Arrays.asList("singing", "running", "swimming")).build();
+        Student s8 = Student.builder().name("Ipho").gender("Female").grade(4).gpa(8.0d).activities(Arrays.asList("cycling", "basketball", "running")).build();
+        Student s9 = Student.builder().name("Mac").gender("Male").grade(5).gpa(7.5d).activities(Arrays.asList("soccer", "crafting", "cooking")).build();
+        Student s0 = Student.builder().name("Cisco").gender("Female").grade(5).gpa(8.0d).activities(Arrays.asList("crafting", "aerobics", "cooking")).build();
         return Arrays.asList(s1, s2, s3, s4, s5, s6, s7, s8, s9, s0);
     }
 }
---
+++ src/test/java/com/learn/java8/lambdasAndFunctionalInterfaces/StudentBiConsumerTest.java
@@ -0,0 +1,35 @@
+package com.learn.java8.lambdasAndFunctionalInterfaces;
+
+import com.learn.java8.bootstrap.StudentBootstrap;
+import com.learn.java8.models.Student;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+@Slf4j
+public class StudentBiConsumerTest {
+
+    private List<Student> students = null;
+    private BiConsumer<String, List<String>> studentBiConsumer = null;
+
+    @BeforeEach
+    void setUp() {
+        students = StudentBootstrap.getStudents();
+        studentBiConsumer = (name, activities) -> log.info(name + "    :   " + activities);
+    }
+
+    @AfterEach
+    void tearDown() {
+        students = null;
+        studentBiConsumer = null;
+    }
+
+    @Test
+    void printStudentActivities() {
+        students.forEach(s -> studentBiConsumer.accept(s.getName(), s.getActivities()));
+    }
+}
