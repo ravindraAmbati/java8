c0dff315ad4b3a6616dfa1fd8f2fe742fbb909da
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-02 10:38:21 +0530

    impl comparator, closes #5

---
+++ src/test/java/com/learn/java8/lambdas/ComparatorImpl.java
@@ -0,0 +1,61 @@
+package com.learn.java8.lambdas;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+@Slf4j
+public class ComparatorImpl {
+
+    private Comparator<Integer> integerComparator = null;
+
+    @BeforeEach
+    void setUp(){
+        integerComparator = null;
+    }
+
+    @AfterEach
+    void tearDown(){
+        integerComparator = null;
+    }
+
+    @Test
+    void compare_Java7(){
+        integerComparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        };
+        log.info("compare_Java7 "+integerComparator.compare(2,3));
+    }
+
+    @Test
+    void compare(){
+        integerComparator = (Integer o1, Integer o2) -> {
+            return o1.compareTo(o2);
+        };
+        log.info("compare "+integerComparator.compare(2,3));
+    }
+
+    @Test
+    void compare_SingleLine(){
+        integerComparator = (Integer o1, Integer o2) ->  o1.compareTo(o2);
+        log.info("compare_SingleLine "+integerComparator.compare(2,3));
+    }
+
+    @Test
+    void compare_SingleLine_NoParameterTypes(){
+        integerComparator = (o1, o2) ->  o1.compareTo(o2);
+        log.info("compare_SingleLine_NoParameterTypes "+integerComparator.compare(2,3));
+    }
+
+    @Test
+    void compare_SingleLine_MethodReference(){
+        integerComparator = Integer::compareTo;
+        log.info("compare_SingleLine_MethodReference "+integerComparator.compare(2,3));
+    }
+}
