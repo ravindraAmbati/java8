d4a48fbfe5155b260e1923eeca17a97c66b0a386
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-06 08:55:28 +0530

    NumericStreams -Int, Long, Double, closes #40

---
+++ src/test/java/com/learn/java8/streams/numericStreams/NumericStreamsIntLongDoubleTest.java
@@ -0,0 +1,449 @@
+package com.learn.java8.streams.numericStreams;
+
+/* @author ravin @date 06-05-2020 @time 08:29 */
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+@Slf4j
+public class NumericStreamsIntLongDoubleTest {
+
+    @Test
+    void printIntStream() {
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .rangeClosed(1, 100)
+                                .count()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .range(1, 100)
+                                .count()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .rangeClosed(1, 100)
+                                .sum()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .range(1, 100)
+                                .sum()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .rangeClosed(1, 100)
+                                .min()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .range(1, 100)
+                                .min()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .rangeClosed(1, 100)
+                                .max()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .range(1, 100)
+                                .max()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .rangeClosed(1, 100)
+                                .average()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .range(1, 100)
+                                .average()
+                                .orElse(0)
+                )
+        );
+//        IntStream
+//                .rangeClosed(1,100)
+//                .forEach(
+//                        i -> log.info(String.valueOf(i))
+//                );
+//        IntStream
+//                .range(1,100)
+//                .forEach(
+//                        i -> log.info(String.valueOf(i))
+//                );
+        log.info(
+                IntStream
+                        .rangeClosed(1, 100)
+                        .summaryStatistics()
+                        .toString()
+        );
+        log.info(
+                IntStream
+                        .range(1, 100)
+                        .summaryStatistics()
+                        .toString()
+        );
+    }
+
+    @Test
+    void printLongStream() {
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .rangeClosed(1, 100)
+                                .count()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .range(1, 100)
+                                .count()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .rangeClosed(1, 100)
+                                .sum()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .range(1, 100)
+                                .sum()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .rangeClosed(1, 100)
+                                .min()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .range(1, 100)
+                                .min()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .rangeClosed(1, 100)
+                                .max()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .range(1, 100)
+                                .max()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .rangeClosed(1, 100)
+                                .average()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .range(1, 100)
+                                .average()
+                                .orElse(0)
+                )
+        );
+//        LongStream
+//                .rangeClosed(1,100)
+//                .forEach(
+//                        i -> log.info(String.valueOf(i))
+//                );
+//        LongStream
+//                .range(1,100)
+//                .forEach(
+//                        i -> log.info(String.valueOf(i))
+//                );
+        log.info(
+                LongStream
+                        .rangeClosed(1, 100)
+                        .summaryStatistics()
+                        .toString()
+        );
+        log.info(
+                LongStream
+                        .range(1, 100)
+                        .summaryStatistics()
+                        .toString()
+        );
+    }
+
+    @Test
+    void printFromLongToDoubleStream() {
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .rangeClosed(1, 100)
+                                .asDoubleStream()
+                                .count()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .range(1, 100)
+                                .asDoubleStream()
+                                .count()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .rangeClosed(1, 100)
+                                .asDoubleStream()
+                                .sum()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .range(1, 100)
+                                .asDoubleStream()
+                                .sum()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .rangeClosed(1, 100)
+                                .asDoubleStream()
+                                .min()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .range(1, 100)
+                                .asDoubleStream()
+                                .min()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .rangeClosed(1, 100)
+                                .asDoubleStream()
+                                .max()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .range(1, 100)
+                                .asDoubleStream()
+                                .max()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .rangeClosed(1, 100)
+                                .asDoubleStream()
+                                .average()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        LongStream
+                                .range(1, 100)
+                                .asDoubleStream()
+                                .average()
+                                .orElse(0)
+                )
+        );
+//        LongStream
+//                .rangeClosed(1,100)
+//        .asDoubleStream()
+//                .forEach(
+//                        i -> log.info(String.valueOf(i))
+//                );
+//        LongStream
+//                .range(1,100)
+//                .asDoubleStream()
+//                .forEach(
+//                        i -> log.info(String.valueOf(i))
+//                );
+        log.info(
+                LongStream
+                        .rangeClosed(1, 100)
+                        .asDoubleStream()
+                        .summaryStatistics()
+                        .toString()
+        );
+        log.info(
+                LongStream
+                        .range(1, 100)
+                        .asDoubleStream()
+                        .summaryStatistics()
+                        .toString()
+        );
+    }
+
+    @Test
+    void printFromIntToDoubleStream() {
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .rangeClosed(1, 100)
+                                .asDoubleStream()
+                                .count()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .range(1, 100)
+                                .asDoubleStream()
+                                .count()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .rangeClosed(1, 100)
+                                .asDoubleStream()
+                                .sum()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .range(1, 100)
+                                .asDoubleStream()
+                                .sum()
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .rangeClosed(1, 100)
+                                .asDoubleStream()
+                                .min()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .range(1, 100)
+                                .asDoubleStream()
+                                .min()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .rangeClosed(1, 100)
+                                .asDoubleStream()
+                                .max()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .range(1, 100)
+                                .asDoubleStream()
+                                .max()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .rangeClosed(1, 100)
+                                .asDoubleStream()
+                                .average()
+                                .orElse(0)
+                )
+        );
+        log.info(
+                String.valueOf(
+                        IntStream
+                                .range(1, 100)
+                                .asDoubleStream()
+                                .average()
+                                .orElse(0)
+                )
+        );
+//        IntStream
+//                .rangeClosed(1,100)
+//        .asDoubleStream()
+//                .forEach(
+//                        i -> log.info(String.valueOf(i))
+//                );
+//        IntStream
+//                .range(1,100)
+//                .asDoubleStream()
+//                .forEach(
+//                        i -> log.info(String.valueOf(i))
+//                );
+        log.info(
+                IntStream
+                        .rangeClosed(1, 100)
+                        .asDoubleStream()
+                        .summaryStatistics()
+                        .toString()
+        );
+        log.info(
+                IntStream
+                        .range(1, 100)
+                        .asDoubleStream()
+                        .summaryStatistics()
+                        .toString()
+        );
+    }
+}
