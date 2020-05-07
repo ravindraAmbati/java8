4e7a8a0706d4a7a5cef2f6138104d5e6acb2b6ee
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-07 20:32:28 +0530

    student names joining, closes #42

---
+++ src/test/java/com/learn/java8/streams/terminalOperations/StreamsJoiningTest.java
@@ -0,0 +1,54 @@
+package com.learn.java8.streams.terminalOperations;
+
+/* @author ravin @date 07-05-2020 @time 20:25 */
+
+import com.learn.java8.bootstrap.StudentBootstrap;
+import com.learn.java8.models.Student;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
+@Slf4j
+public class StreamsJoiningTest {
+
+    private List<Student> students = null;
+
+    @BeforeEach
+    void setUp() {
+        students = StudentBootstrap.getStudents();
+    }
+
+    @AfterEach
+    void tearDown() {
+        students = null;
+    }
+
+    @Test
+    void print() {
+
+        log.info(
+                students
+                        .stream()
+                        .map(Student::getName)
+                        .collect(joining())
+        );
+        log.info(
+                students
+                        .stream()
+                        .map(Student::getName)
+                        .collect(joining("||"))
+        );
+        log.info(
+                students
+                        .stream()
+                        .map(Student::getName)
+                        .collect(joining(",", "[", "]"))
+        );
+
+    }
+}
