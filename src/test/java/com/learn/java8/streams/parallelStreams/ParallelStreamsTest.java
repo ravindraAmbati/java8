a6cd86c2816c844cdaf0abbe4ba13e72627fe11a
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-09 16:33:06 +0530

    parallel streams, closes #49

---
+++ src/test/java/com/learn/java8/streams/parallelStreams/ParallelStreamsTest.java
@@ -0,0 +1,60 @@
+package com.learn.java8.streams.parallelStreams;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+/* @author ravin @date 09-05-2020 @time 16:20 */
+@Slf4j
+public class ParallelStreamsTest {
+
+    private long startTime = 0L;
+    private long endTime = 0L;
+
+    @BeforeEach
+    void setUp() {
+        startTime = System.currentTimeMillis();
+    }
+
+    @AfterEach
+    void tearDown() {
+        endTime = System.currentTimeMillis();
+        log.info("total ms taken:" + (endTime - startTime));
+        startTime = 0L;
+        endTime = 0L;
+    }
+
+    @Test
+    void streams() {
+        int i = 0;
+        while (i < 100) {
+            log.info(
+                    IntStream
+                            .rangeClosed(0, 100000)
+                            .summaryStatistics()
+                            .toString()
+            );
+            i++;
+        }
+
+    }
+
+    @Test
+    void parallelStreams() {
+        int i = 0;
+        while (i < 100) {
+            log.info(
+                    IntStream
+                            .rangeClosed(0, 100000)
+                            .parallel()
+                            .summaryStatistics()
+                            .toString()
+            );
+            i++;
+        }
+
+    }
+}
